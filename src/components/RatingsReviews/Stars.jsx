5daf0521151a2815e02c5b00e804a139c3e01fe3
Tyler Moniz <tmgnbvt@gmail.com>
2021-04-19 09:15:37 -0400

    Test Commit

---
+++ src/components/RatingsReviews/Stars.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import axios from 'axios';
+// import ReactDOM from 'react-dom';
+
+
+
+class Stars extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    },
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Ratings Reviews</h1>
+      </div>
+    );
+  }
+}
+
+export default Stars;
